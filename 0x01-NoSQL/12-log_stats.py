d0f9a32a9d78aa91049454b4e446b51e95ebf282
gabrielhyimah <gabrielamanfogyimah@gmail.com>
2023-12-19 22:15:15 +0000

    Added the Ngix Log module

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -4,19 +4,20 @@
 
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://localhost:27017/')
-db = client.logs
-collection = db.nginx
+client = MongoClient('localhost', 27017)
+db = client['logs']
+collection = db['nginx']
 
-total = collection.count_documents({})
+total_logs = collection.count_documents({})
+print(f"first line: {total_logs} logs where {total_logs} is the number of documents in this collection")
 
-methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-methods_counts = [collection.count_documents({"method": method}) for method in methods]
+http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+print("second line: Methods:")
+for method in http_methods:
+    count = collection.count_documents({"method": method})
+    print(f"\t{count} {method} requests")
 
-path_count = collection.count_documents({"method": "GET", "path": "/status"})
+status_count = collection.count_documents({"method": "GET", "path": "/status"})
+print(f"one line: {status_count} documents with method=GET and path=/status")
 
-print(f"{total} logs")
-print("Methods:")
-for method, count in zip(methods, methods_counts):
-    print(f"\tmethod {method}: {count}")
-print(f"{path_count} status check")
+client.close()
