a1aa38db4b8795749e778577aea72a5e1427a783
gabrielhyimah <gabrielamanfogyimah@gmail.com>
2023-12-20 21:06:54 +0000

    Added a cache class

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Cache Class"""
+from ctypes import Union
+import uuid
+import redis
+
+
+class Cache():
+    """Cache class"""
+
+    def __init__(self) -> None:
+        """Initializes an instance of the cache class"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self: any, data: Union[str, bytes, int, float]) -> str:
+        """"
+        generates a random key (e.g. using uuid)
+        store the input data in Redis using the random key
+        returns key
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
